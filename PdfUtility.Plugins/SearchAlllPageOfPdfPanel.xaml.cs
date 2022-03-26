8e221f9da24a6e16c51bd6b407718289c9a17ac0
hoge8086 <hoge8086@gmail.com>
2022-03-27 00:14:32 +0900

    全ページ検索機能を追加

---
+++ PdfUtility.Plugins/SearchAlllPageOfPdfPanel.xaml.cs
@@ -0,0 +1,44 @@
+﻿using PanelPluginInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PdfUtility.Plugins
+{
+    /// <summary>
+    /// SearchAlllPageOfPdfPanel.xaml の相互作用ロジック
+    /// </summary>
+    public partial class SearchAlllPageOfPdfPanel : UserControl, IPanelPlugin
+    {
+        public string PluginName => "PDF検索(全ページ)";
+
+        public UserControl Panel => this;
+        public SearchAlllPageOfPdfPanel(IPluginHost host)
+        {
+            InitializeComponent();
+            this.DataContext = new SearchAlllPageOfPdfPanelViewModel(host)
+            {
+
+                ShowPdf = (string pdfPath, int page) =>
+                {
+
+                    var browser = new BrowsPdfWindow();
+                    browser.Owner = Window.GetWindow(this);
+                    browser.Show();
+                    browser.ShowPdf(pdfPath, page);
+                }
+            };
+        }
+    }
+}
---
+++ PdfUtility.Plugins/SearchAlllPageOfPdfPanelViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Reactive;
+using PanelPluginInterface;
+using PdfUtility.Business;
+using PdfUtility.Infrastructure;
+using Reactive.Bindings;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+
+namespace PdfUtility.Plugins
+{
+    public class PageResult
+    {
+        public int Page { get; set; }
+        public string Word { get; set; }
+    }
+
+    public class SearchAlllPageOfPdfPanelViewModel
+    {
+        public IPluginHost Host;
+        public ReactiveProperty<string> PdfFilePath { get; private set; }
+        public ReactiveProperty<string> Keyword { get; private set; }
+        public ObservableCollection<PageResult> Results { get; private set; }
+        public bool EnableRegexp { get; set; }
+        public ReactiveCommand SearchCommand { get; }
+        public ReactiveCommand<PageResult> ShowPdfCommand { get; }
+        public Action<string, int>  ShowPdf{ get; set; }
+
+        private SearchPdfService searchPdfService = new SearchPdfService();
+        private PdfService pdfService = new PdfService();
+
+        public SearchAlllPageOfPdfPanelViewModel(IPluginHost host)
+        {
+            Host = host;
+            PdfFilePath = new ReactiveProperty<string>();
+            Keyword = new ReactiveProperty<string>();
+            EnableRegexp = false;
+            Results = new ObservableCollection<PageResult>();
+            SearchCommand = new ReactiveCommand();
+            SearchCommand.Subscribe(() =>
+            {
+                try
+                {
+                    var target = new List<SearchTarget> { new SearchTarget(Keyword.Value, EnableRegexp, true) };
+                    searchPdfService.Search(target, PdfFilePath.Value);
+
+                    Results.Clear();
+                    foreach(var hit in target[0].Hits)
+                    {
+                        Results.Add(new PageResult { Page = hit.Page, Word = hit.Text });
+                    }
+
+                    if(Results.Count == 0)
+                        MessageBox.Show("キーワードは見つかりませんでした。", "エラー");
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "エラー");
+                }
+            });
+            ShowPdfCommand = new ReactiveCommand<PageResult>();
+            ShowPdfCommand.Subscribe((x) =>
+            {
+                try
+                {
+                    ShowPdf(PdfFilePath.Value, x.Page);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "エラー");
+                }
+            });
+        }
+    }
+}
